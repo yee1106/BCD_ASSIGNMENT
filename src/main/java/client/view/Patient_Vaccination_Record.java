9600d473fd5e67f9e9f0c008f94436cc9f181584
Chew Chang Wang <chewchangwang@gmail.com>
2021-11-23 00:13:56 +0800

    Change Text Field to Combo Box

--- src/main/java/client/view/Patient_Vaccination_Record.java
+++ src/main/java/client/view/Patient_Vaccination_Record.java
@@ -45,13 +45,13 @@ public class Patient_Vaccination_Record extends javax.swing.JFrame {
     jLabel6 = new javax.swing.JLabel();
     jLabel7 = new javax.swing.JLabel();
     jPanel3 = new javax.swing.JPanel();
-    icTextField = new javax.swing.JTextField();
     dateVaccinatedDateChooser = new com.toedter.calendar.JDateChooser();
     typeVaccineTextField = new javax.swing.JTextField();
     batchIDTextField = new javax.swing.JTextField();
     witnessNameTextField = new javax.swing.JTextField();
     jScrollPane2 = new javax.swing.JScrollPane();
     facilityTextArea = new javax.swing.JTextArea();
+    icComboBox = new javax.swing.JComboBox<>();
     confirmButton = new javax.swing.JButton();
 
     setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -129,13 +129,6 @@ public class Patient_Vaccination_Record extends javax.swing.JFrame {
 
     jPanel3.setBackground(new java.awt.Color(51, 51, 51));
 
-    icTextField.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-    icTextField.addKeyListener(new java.awt.event.KeyAdapter() {
-      public void keyTyped(java.awt.event.KeyEvent evt) {
-        icTextFieldKeyTyped(evt);
-      }
-    });
-
     dateVaccinatedDateChooser.setDateFormatString("yyyy-MM-dd HH:mm:ss");
     dateVaccinatedDateChooser.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
 
@@ -153,22 +146,24 @@ public class Patient_Vaccination_Record extends javax.swing.JFrame {
     facilityTextArea.setRows(5);
     jScrollPane2.setViewportView(facilityTextArea);
 
+    icComboBox.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+
     javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
     jPanel3.setLayout(jPanel3Layout);
     jPanel3Layout.setHorizontalGroup(
       jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-      .addComponent(icTextField)
       .addComponent(dateVaccinatedDateChooser, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
       .addComponent(typeVaccineTextField)
       .addComponent(batchIDTextField)
       .addComponent(witnessNameTextField)
       .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 338, Short.MAX_VALUE)
+      .addComponent(icComboBox, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
     );
     jPanel3Layout.setVerticalGroup(
       jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
       .addGroup(jPanel3Layout.createSequentialGroup()
-        .addComponent(icTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
-        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+        .addComponent(icComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+        .addGap(18, 18, 18)
         .addComponent(dateVaccinatedDateChooser, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
         .addComponent(typeVaccineTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 55, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -249,14 +244,6 @@ public class Patient_Vaccination_Record extends javax.swing.JFrame {
     this.setVisible(false);
   }//GEN-LAST:event_goBackLableMouseReleased
 
-  private void icTextFieldKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_icTextFieldKeyTyped
-    char character = evt.getKeyChar();
-    //input validation
-    if(!(Character.isDigit(character) || (character == KeyEvent.VK_BACK_SPACE) || (character == KeyEvent.VK_DELETE))){
-      evt.consume();
-    }
-  }//GEN-LAST:event_icTextFieldKeyTyped
-
   private void confirmButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_confirmButtonActionPerformed
     if(!isTextFieldEmpty()){
       
@@ -267,7 +254,7 @@ public class Patient_Vaccination_Record extends javax.swing.JFrame {
   }//GEN-LAST:event_confirmButtonActionPerformed
 
   private boolean isTextFieldEmpty(){
-    return (icTextField.getText().isBlank() || typeVaccineTextField.getText().isBlank() || batchIDTextField.getText().isBlank() || witnessNameTextField.getText().isBlank() || facilityTextArea.getText().isBlank() || !dateVaccinatedDateChooser.isValid() || dateFormat.format(dateVaccinatedDateChooser.getDate()).isBlank());
+    return (icComboBox.getSelectedItem() == null || typeVaccineTextField.getText().isBlank() || batchIDTextField.getText().isBlank() || witnessNameTextField.getText().isBlank() || facilityTextArea.getText().isBlank() || !dateVaccinatedDateChooser.isValid() || dateFormat.format(dateVaccinatedDateChooser.getDate()).isBlank());
   }
   
   public void setDefaultDate(){
@@ -315,7 +302,7 @@ public class Patient_Vaccination_Record extends javax.swing.JFrame {
   private com.toedter.calendar.JDateChooser dateVaccinatedDateChooser;
   private javax.swing.JTextArea facilityTextArea;
   private javax.swing.JLabel goBackLable;
-  private javax.swing.JTextField icTextField;
+  private javax.swing.JComboBox<String> icComboBox;
   private javax.swing.JLabel jLabel1;
   private javax.swing.JLabel jLabel2;
   private javax.swing.JLabel jLabel3;
